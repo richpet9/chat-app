937369fd204b17e76753564aed4e5e01d4ee38f8
richpet9 <richpet9@gmail.com>
2020-06-28 13:14:35 -0400

    spam blocker / performance saver

--- frontend/components/MessageInput/index.js
+++ frontend/components/MessageInput/index.js
@@ -1,9 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import "./index.scss";
 
 const MessageInput = ({ sendMessage }) => {
     const [message, setMessage] = useState("");
+    const [spam, setSpam] = useState(false);
+    const [msgCooldown, setMsgCooldown] = useState(0);
+
+    const submitMessage = () => {
+        if (msgCooldown > 0) {
+            setSpam(true);
+        } else {
+            sendMessage(message);
+            setMessage("");
+            setMsgCooldown(4);
+        }
+    };
+
+    useEffect(() => {
+        if (msgCooldown == 0) {
+            setSpam(false);
+            return;
+        }
+
+        setTimeout(() => {
+            setMsgCooldown(msgCooldown - 1);
+        }, 1000);
+    }, [msgCooldown]);
 
     return (
         <div className="message-input-container" id="message-input-bar">
@@ -16,20 +39,22 @@ const MessageInput = ({ sendMessage }) => {
                     if (e.keyCode == 13) {
                         // Pressed enter
                         e.preventDefault();
-                        sendMessage(message);
-                        setMessage("");
+                        submitMessage();
                     }
                 }}
             />
             <button
                 onClick={(e) => {
                     e.preventDefault();
-                    sendMessage(message);
-                    setMessage("");
+                    submitMessage();
                 }}
             >
                 <i className="fas fa-paper-plane"></i>
             </button>
+
+            {spam && (
+                <span className="message-cooldown">{`Please wait ${msgCooldown} more seconds before sending another message.`}</span>
+            )}
         </div>
     );
 };
